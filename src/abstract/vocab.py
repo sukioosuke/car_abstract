50cab33817350deead239e5f43e3851f06839e0e
JTWM\pc <497326406@qq.com>
2019-12-02 08:52:41 +0800

    fix

--- src/abstract/vocab.py
+++ src/abstract/vocab.py
@@ -1,38 +1,65 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
+import jieba
 import pkuseg
 import re
+import datetime
 
-from multiprocessing import Pool, Process, Manager
+from multiprocessing import Pool, cpu_count, Manager
 from utils.paths import *
 
 
-class Vocab(object):
+class Vocab:
     """get vocabulary in test and train data
     1. filter stopwords
     2. filter punctuation
     3. generate words dictionary
     """
-    manager = Manager()
-    lock = manager.Lock()
+    __manager = None
+    __lock = None
+    __MIN_COUNT = 5
+    __multi = False
+    __package = "jieba"
+    __partition = 1
     stop_words = []
     less_use = []
-    vocabulary = manager.dict()
-    MIN_COUNT = 5
+    vocabulary = {}
 
-    def __init__(self):
-        file = open('../../data/stop_words.txt', mode='r')
+    def __init__(self, multi=False, package="jieba"):
+        file = open(stop_words_path, mode='r', encoding='utf-8')
         for i in file.readlines():
             self.stop_words.append(i.replace('\n', ''))
+        if multi:
+            self.vocabulary = self.__manager.dict()
+            self.multi = multi
+            self.__manager = Manager()
+            self.__lock = self.__manager.Lock()
+            self.__partition = cpu_count()
+        self.__package = package
 
     def cut_words(self, sentence):
         cut = []
-        seg = pkuseg.pkuseg(model_name='default', user_dict=user_dict_path)
+        seg = None
         clean = re.sub(
             r'[\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]|[\s+\-\|\!\/\[\]\{\}_,.$%^*(+\"\')]+|[:：+——()?【】“”！，。？、~@#￥%……&*（）]+|车主说|技师说|语音|图片|你好|您好',
             ' ', sentence)
-        for j in seg.cut(clean):
-            with self.lock:
+        if self.__package == "jieba":
+            jieba.load_userdict(user_dict_path)
+            seg = jieba.cut(clean)
+        elif self.__package == "pkuseg":
+            s = pkuseg.pkuseg(model_name='default', user_dict=user_dict_path)
+            seg = s.cut(clean)
+        for j in seg:
+            if self.__multi:
+                with self.lock:
+                    if j in self.vocabulary.keys():
+                        self.vocabulary[j] += 1
+                        cut.append(j)
+                    elif j not in self.stop_words:
+                        self.vocabulary[j] = 1
+                        cut.append(j)
+            else:
                 if j in self.vocabulary.keys():
                     self.vocabulary[j] += 1
                     cut.append(j)
@@ -41,12 +68,30 @@ class Vocab(object):
                     cut.append(j)
         return ' '.join(cut)
 
+    def set_vocabulary(self, dataframe):
+        if self.__multi:
+            pool = Pool(self.__partition)
+            data_split = np.array_split(dataframe, self.__partition)
+            data = pd.concat(pool.map(self.dataframe_cut, data_split))
+            pool.close()
+            pool.join()
+            self.update_lessuse()
+            data_split = np.array_split(data, self.__partition)
+            data = pd.concat(pool.map(self.dataframe_filter, data_split))
+            pool.close()
+            pool.join()
+            return data
+        else:
+            df = self.dataframe_cut(dataframe)
+            self.update_lessuse()
+            return self.dataframe_filter(df)
+
     def filter_lessuse(self, sentence):
-        ' '.join(sentence.split(' ').filter(lambda x: x in self.less_use))
+        ' '.join(filter(lambda x: x not in self.less_use, sentence.split(' ')))
 
     def update_lessuse(self):
         for word in self.vocabulary.keys():
-            if self.vocabulary.get(word) < self.MIN_COUNT:
+            if self.vocabulary.get(word) < self.__MIN_COUNT:
                 self.less_use.append(word)
         for i in self.less_use: self.vocabulary.pop(i)
 
@@ -60,26 +105,10 @@ class Vocab(object):
             df[col_name] = df[col_name].apply(self.filter_lessuse)
         return df
 
-    def multi_set_vocabulary(self, dataframe, partitions=8):
-        df = pd.DataFrame
-        pool = Pool(partitions)
-        data_split = np.array_split(dataframe, partitions)
-        data = pd.concat(pool.map(self.dataframe_cut, data_split))
-        self.update_lessuse()
-        data_split = np.array_split(data, partitions)
-        data = pd.concat(pool.map(self.dataframe_filter, data_split))
-        pool.close()
-        pool.join()
-        return data
-
-    def set_vocabulary(self, dataframe):
-        df = self.dataframe_cut(dataframe)
-        self.update_lessuse()
-        return self.dataframe_filter(df)
-
 
 if __name__ == '__main__':
     v = Vocab()
     train_file = pd.read_csv(train_data_path, encoding='utf-8')
-    v.multi_set_vocabulary(train_file.head(24))
+    train_content = train_file.dropna().drop("QID", axis=1)
+    v.set_vocabulary(train_file)
     print(v.vocabulary)
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+import time
 from multiprocessing import cpu_count, Pool
 
-# cpu 数量
-cores = cpu_count()
 # 分块个数
-partitions = cores
+partitions = cpu_count()
 
 
 def parallelize(df, func):
@@ -19,3 +18,10 @@ def parallelize(df, func):
     pool.close()
     pool.join()
     return data
+
+
+def get_running_time(func, *args, **kwargs):
+    start = time.clock()
+    func(*args, **kwargs)
+    end = time.clock()
+    print("using time: " + str(start - end))
